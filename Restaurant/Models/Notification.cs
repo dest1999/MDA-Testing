3c97f347eb6673900c4035adfba6d11a13373014
Oleg <serv_all@mail.ru>
2023-01-29 17:46:03 +0300

    Отправка уведомлений выделена в асинхронный метод в класс Notification

---
+++ Restaurant/Models/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    internal class Notification
+    {
+        public static void SendNotifyAsync(string Message)
+        {
+            Task.Run(() =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Message from Notification Class:\n{Message}");
+                Console.ResetColor();
+            });
+        }
+    }
+}
